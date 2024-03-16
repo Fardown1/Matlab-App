b0ff82b0fee1f738c83546b7b344ba984a8d9918
Berkin Günal <34184566+Fardown1@users.noreply.github.com>
2024-03-16 22:09:12 +0300

    Add files via upload

---
+++ siralaMatris.m
@@ -0,0 +1,18 @@
+function siraliMatris = siralaMatris(matris)
+
+    [m, n] = size(matris);
+    
+    % Matrisi tek bir vektöre dönüþtür
+    vektorMatris = matris(:);
+    
+    % Küçükten büyüðe sýrala
+    siraliVektor = sort(vektorMatris);
+    
+    % Sýralý vektörü tekrar m satýr ve n sütunlu matrise dönüþtür
+    siraliMatris = zeros(m, n);
+    k = 1;
+    
+        end
+    end
+    
+end
