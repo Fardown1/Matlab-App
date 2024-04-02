8a0530c931fcd2810004fde409296486c8f91364
Berkin Günal <34184566+Fardown1@users.noreply.github.com>
2024-04-02 19:54:15 +0300

    Add files via upload

---
+++ mukemmel_mi.m
@@ -0,0 +1,11 @@
+function sonuc = mukemmel_mi(n)
+if sum(bolenler(n))==2*n
+    fprintf("mükemmeldir");
+else
+    fprintf("mükemmel deðildir");
+end
+end
+
+
+
+% kendisi hariç bölenler toplamý kendisini veren sayýlar
