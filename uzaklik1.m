14818a124424cc5ed6f9383fecadb618da69f598
Berkin Günal <34184566+Fardown1@users.noreply.github.com>
2024-04-02 19:55:20 +0300

    Add files via upload

---
+++ uzaklik1.m
@@ -0,0 +1,13 @@
+function sonuc = uzaklik1([x1,y1],[x2,y2])
+% P1 = (x1,y1) ve P2 = (x2,y2)
+% noktalarý arasýndaki uzaklýðý bulan fonksiyon
+% Örnek P1 = (3,5) ve P2 = (2,7) için kullaným þekli :
+% uzaklik2([3,5],[2,7]
+x1 = P1(1);
+y1 = P1(2);
+x2 = P2(1);
+y2 = P2(2);
+
+sonuc = sqrt((x2-x1)^2+(y2-y1))
+
+end
